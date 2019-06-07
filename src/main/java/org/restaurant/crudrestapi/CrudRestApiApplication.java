877ac1c01ce7cfb5f4e29a7d8722b54f2a41e42f
rublin <toor.ua@gmail.com>
2019-06-07 16:36:51 +0300

    boot init

---
+++ src/main/java/org/restaurant/crudrestapi/CrudRestApiApplication.java
@@ -0,0 +1,12 @@
+package org.restaurant.crudrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRestApiApplication.class, args);
+    }
+}
